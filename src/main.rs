a4dd393bfcfeb6b4effa6f245f96b63bb6b8eb15
DovaPT <peterd5590@gmail.com>
2025-03-30 03:10:54 +0200

    implemented Vec3

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use core::f64;
+
 fn main() {
     const IMAGE_WIDTH: i32 = 1920;
     const IMAGE_HEIGHT: i32 = 1080;
     std::print!("P3\n {} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
     for j in 0..IMAGE_HEIGHT {
+        eprint!("\rScanlines remaining: {} ", (IMAGE_HEIGHT - j));
         for i in 0..IMAGE_WIDTH {
             let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
             let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
@@ -14,5 +17,153 @@ fn main() {
 
             std::print!("{} {} {}\n", ir, ig, ib)
         }
+    }
+    eprint!("{:<23}", "\rDone")
+}
+
+#[derive(Clone, Copy)]
+struct Vec3 {
+    e: [f64; 3],
+}
+
+impl Vec3 {
+    pub fn new() -> Self {
+        Vec3 { e: [0.0, 0.0, 0.0] }
+    }
+    pub fn len(self) -> f64 {
+        self.len_sqrt().sqrt()
+    }
+
+    pub fn len_sqrt(self) -> f64 {
+        self[0] * self[0] + self[1] * self[1] + self[2] * self[2]
+    }
+
+    pub fn dot(self, rhs: Self) -> f64 {
+        self[0] * rhs[0] + self[1] * rhs[1] + self[2] * rhs[2]
+    }
+
+    pub fn cross(self, rhs: Self) -> Self {
+        Self {
+            e: [
+                self[1] * rhs[2] - self[2] * rhs[1],
+                self[2] * rhs[0] - self[0] * rhs[2],
+                self[0] * rhs[1] - self[1] * rhs[0],
+            ],
+        }
+    }
+
+    pub fn unit_vector(self) -> Self {
+        self / self.len()
+    }
+}
+
+impl std::ops::Index<usize> for Vec3 {
+    type Output = f64;
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.e[index]
+    }
+}
+
+impl std::ops::IndexMut<usize> for Vec3 {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.e[index]
+    }
+}
+impl std::fmt::Display for Vec3 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} {}", self[0], self[1], self[3])
+    }
+}
+impl std::ops::Neg for Vec3 {
+    type Output = Self;
+    fn neg(self) -> Self::Output {
+        Self {
+            e: [-self[0], -self[1], -self[2]],
+        }
+    }
+}
+
+impl std::ops::Add for Vec3 {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            e: [self[0] + rhs[0], self[1] + rhs[1], self[2] + rhs[2]],
+        }
+    }
+}
+
+impl std::ops::AddAssign for Vec3 {
+    fn add_assign(&mut self, rhs: Self) {
+        self[0] += rhs[0];
+        self[1] += rhs[1];
+        self[2] += rhs[2];
+    }
+}
+
+impl std::ops::Sub for Vec3 {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            e: [self[0] - rhs[0], self[1] - rhs[1], self[2] - rhs[2]],
+        }
+    }
+}
+
+impl std::ops::Mul for Vec3 {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self {
+            e: [self[0] * rhs[0], self[1] * rhs[1], self[2] * rhs[2]],
+        }
+    }
+}
+
+impl std::ops::Mul<f64> for Vec3 {
+    type Output = Self;
+    fn mul(self, rhs: f64) -> Self::Output {
+        Self {
+            e: [self[0] * rhs, self[1] * rhs, self[2] * rhs],
+        }
+    }
+}
+
+impl std::ops::MulAssign for Vec3 {
+    fn mul_assign(&mut self, rhs: Self) {
+        self[0] *= rhs[0];
+        self[1] *= rhs[1];
+        self[2] *= rhs[2];
+    }
+}
+
+impl std::ops::MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        self[0] *= rhs;
+        self[1] *= rhs;
+        self[2] *= rhs;
+    }
+}
+
+impl std::ops::Div<f64> for Vec3 {
+    type Output = Self;
+    fn div(self, rhs: f64) -> Self::Output {
+        Self {
+            e: [self[0] / rhs, self[1] / rhs, self[2] / rhs],
+        }
+    }
+}
+
+impl std::ops::DivAssign for Vec3 {
+    fn div_assign(&mut self, rhs: Self) {
+        self[0] /= rhs[0];
+        self[1] /= rhs[1];
+        self[2] /= rhs[2];
+    }
+}
+
+impl std::ops::DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, rhs: f64) {
+        self[0] /= rhs;
+        self[1] /= rhs;
+        self[2] /= rhs;
     }
 }

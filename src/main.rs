5aa89c3fd6a2c70702f777314e6802087566c132
DovaPT <peterd5590@gmail.com>
2025-04-20 11:26:40 +0200

    removed unneeded arguments

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,8 @@ use std::time;
 
 fn main() {
     let timer: time::Instant = time::Instant::now();
-
-    let mut image_file: std::fs::File =
-        std::fs::File::create("image.ppm").expect("cant create image.ppm");
     
-    rtiaw::scenes::scene2(&mut image_file);
+    rtiaw::scenes::scene2();
 
     let time_elapsed: time::Duration = timer.elapsed();
     let s: String = format!("\rIt took {} seconds", time_elapsed.as_secs_f64());
